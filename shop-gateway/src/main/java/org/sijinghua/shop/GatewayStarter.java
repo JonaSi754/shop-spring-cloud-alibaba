2120fb2e01c713cebd276fac1f76d02e8e4e67c9
JonaSi754 <qyssjh@163.com>
2023-04-21 17:07:23 +0800

    服务网关

---
+++ shop-gateway/src/main/java/org/sijinghua/shop/GatewayStarter.java
@@ -0,0 +1,14 @@
+package org.sijinghua.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class GatewayStarter {
+    public static void main(String[] args) {
+        System.setProperty("csp.sentinel.app.type", "1");
+        SpringApplication.run(GatewayStarter.class, args);
+    }
+}
